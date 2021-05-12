7a8b1feab90b080f455d6e058cde0d45d542e678
YanaNepik <76006872+YanaNepik@users.noreply.github.com>
2021-05-12 19:53:48 +0300

    Непомнящая

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -5,7 +5,63 @@
 
 template<typename T>
 class TPQueue {
-  // Сюда помещается описание структуры "Очередь с приоритетами"
+ private:
+  struct ITEM {
+    T data;
+    ITEM* prior;
+    ITEM* next;
+  };
+  ITEM* start;
+  ITEM* end;
+  TPQueue::ITEM* create(const T& data, ITEM* prior) {
+    ITEM* item = new ITEM;
+    item->data = data;
+    item->next = nullptr;
+    item->prior = prior;
+    return item;
+  }
+
+ public:
+  TPQueue() : start(nullptr), end(nullptr) {}
+  ~TPQueue() {
+    while (start) pop();
+  }
+  void push(const T& data) {
+    if (end && start) {
+      ITEM* this = end;
+      while (this && data.prior > (this->data).prior) {
+        this = this->prior;
+      }
+      if (this) {
+        ITEM* tmp = this->next;
+        this->next = create(data, this);
+        this = this->next;
+        this->next = tmp;
+        if (tmp) {
+          tmp->prior = this;
+        } else {
+          end = this;
+        }
+      } else {
+        this = create(data, nullptr);
+        this->next = start;
+        start->prior = this;
+        start = this;
+      }
+    } else {
+      start = create(data, nullptr);
+      end = start;
+    }
+  }
+  T pop() {
+    assert(start);
+    ITEM* tmp = start->next;
+    T data = start->data;
+    if (tmp) tmp->prior = nullptr;
+    delete start;
+    start = tmp;
+    return data;
+  }
 };
 
 struct SYM {
